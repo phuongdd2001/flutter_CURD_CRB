41338b97fd50008b5701f260fba2c4f174f5507f
phuong1772001 <phuongdd@volio.vn>
2023-08-17 11:25:15 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'insert.dart';
+import 'update.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+          apiKey: "AIzaSyB_1JiSSJ5kRKzGajCXg5IdJyi0PKo1kiY",
+            appId: "1:957892027188:android:7103f4c2b2c40a7384bb61",
+          messagingSenderId: "957892027188",
+          projectId: "flutterrealdb",
+          databaseURL: "https://flutterrealdb-default-rtdb.firebaseio.com",
+        ));
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'realtime CRUD',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    DatabaseReference db_Ref =
+    FirebaseDatabase.instance.ref().child('contacts');
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigo[900],
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => ccreate(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Contacts',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: FirebaseAnimatedList(
+        query: db_Ref,
+        shrinkWrap: true,
+        itemBuilder: (context, snapshot, animation, index) {
+          Map Contact = snapshot.value as Map;
+          Contact['key'] = snapshot.key;
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => UpdateRecord(
+                    Contact_Key: Contact['key'],
+                  ),
+                ),
+              );
+              // print(Contact['key']);
+            },
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  tileColor: Colors.indigo[100],
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.red[900],
+                    ),
+                    onPressed: () {
+                      db_Ref.child(Contact['key']).remove();
+                    },
+                  ),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      Contact['url'],
+                    ),
+                  ),
+                  title: Text(
+                    Contact['name'],
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    Contact['number'],
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
